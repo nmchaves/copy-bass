a6bd3d10d2d85fcc6c166dbf9c77a6cddc91a12a
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-09 13:29:07 -0700

    Add a basic 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <h2 className="text-2xl font-semibold mb-8">Not Found</h2>
+      <p className="mb-8">Couldn&apos;t find what you&apos;re looking for.</p>
+      <Link href="/">Back to Home</Link>
+    </main>
+  );
+}
