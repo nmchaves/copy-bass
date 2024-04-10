f73db5e3c88907ac280c1676ecce0633b981bc2e
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-09 18:04:21 -0700

    Allow the user to adjust the playback speed more conveniently

--- app/song/[id]/Player.tsx
+++ app/song/[id]/Player.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import { useState } from "react";
 import ReactPlayer from "react-player/youtube";
 import { ClientOnly } from "@/components/ui/ClientOnly";
+import { Slider } from "@/components/ui/Slider";
 
 export const Player: React.FC<{ url: string }> = ({ url }) => {
+  // The YouTube player UI already allows the user to customize the playback
+  // speed. But I don't like repeatedly opening/closing those settings while I'm
+  // practicing.
+  // TODO: If the user manually changes the playback speed using the player,
+  // update this React state to stay in sync.
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const playbackRateInputId = `playbackRate-${url}`;
+
   // Note: react-player doesn't support SSR. See:
   // https://github.com/cookpete/react-player/issues/1474#issuecomment-1184645105
   // That solution recommends using next/dynamic, but it's simpler to use the
@@ -12,7 +22,28 @@ export const Player: React.FC<{ url: string }> = ({ url }) => {
   // https://github.com/cookpete/react-player/issues/1455#issuecomment-1207154843
   return (
     <ClientOnly>
-      <ReactPlayer url={url} controls={true} />
+      <ReactPlayer url={url} controls={true} playbackRate={playbackRate} />
+
+      <div className="w-1/2 mx-auto mt-2">
+        <label className="block mb-1" htmlFor={playbackRateInputId}>
+          Playback Rate
+        </label>
+        <div className="flex">
+          <Slider
+            id={playbackRateInputId}
+            value={[playbackRate]}
+            min={0.1}
+            max={2}
+            step={0.1}
+            onValueChange={([newValue]) => {
+              setPlaybackRate(newValue);
+            }}
+          />
+          <span className="ml-2 font-medium" aria-label="Playback Rate Value">
+            {playbackRate}
+          </span>
+        </div>
+      </div>
     </ClientOnly>
   );
 };
