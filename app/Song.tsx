7e3253654567abdb7830c208d16879c9494e976d
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-11 00:49:27 -0700

    Make the hover state for songs feel a little nicer by darkening the border on hover and adding a CSS transition

--- app/Song.tsx
+++ app/Song.tsx
@@ -5,7 +5,7 @@ export const Song: React.FC<{ song: SongMetadata }> = ({ song }) => {
   return (
     <Link
       href={`/song/${song.id}`}
-      className="block p-4 border border-slate-300 rounded-xl bg-slate-50 hover:bg-slate-100"
+      className="block p-4 transition-colors border border-slate-300 hover:border-slate-400 rounded-xl bg-slate-50 hover:bg-slate-200"
     >
       <p className="text-lg font-medium mb-1" aria-label="Song Title">
         {song.title}
