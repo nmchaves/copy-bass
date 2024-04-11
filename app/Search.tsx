9fc87468c43663929c5c305ae6a9aa68e8dc2c8a
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-11 00:33:38 -0700

    Allow user to filter songs by title/artist

---
+++ app/Search.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { Input } from "@/components/ui/Input";
+
+const searchQueryKey = "query";
+export type SearchQueryKey = typeof searchQueryKey;
+
+export const Search = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(newQuery: string) {
+    const params = new URLSearchParams(searchParams);
+    if (newQuery) {
+      params.set(searchQueryKey, newQuery);
+    } else {
+      params.delete(searchQueryKey);
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  return (
+    <Input
+      placeholder="Filter by title/artist"
+      defaultValue={searchParams.get(searchQueryKey)?.toString()}
+      onChange={(e) => handleSearch(e.target.value)}
+      className="max-w-md mb-4"
+    />
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
 import { songs } from "@/lib/songs";
 import { Song } from "./Song";
+import { Search, SearchQueryKey } from "./Search";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: Partial<Record<SearchQueryKey, string>>;
+}) {
+  const query = searchParams?.query;
+
+  let filteredSongs = songs;
+  if (query) {
+    const lowerQuery = query.toLowerCase();
+    filteredSongs = songs.filter(
+      (song) =>
+        song.title.toLowerCase().includes(lowerQuery) ||
+        song.artist.toLowerCase().includes(lowerQuery),
+    );
+  }
+
   return (
     <main className="flex flex-col items-center">
       <h1 className="text-3xl font-semibold mb-4">
         Nico&apos;s Music Practice 🎸
       </h1>
-      <h2 className="text-xl font-semibold mb-2">Songs</h2>
+      <h2 className="text-xl font-semibold mb-4">Songs</h2>
+      <Search />
       <div className="max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {songs.map((song) => (
+        {filteredSongs.map((song) => (
           <div key={song.title} className="m-4">
             <Song song={song} />
           </div>
