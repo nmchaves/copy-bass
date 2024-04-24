765b6ded0f98f949a28ba6c13ba82fda9b1c62de
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-24 16:55:58 -0700

    Update site title/description

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Music Practice App",
-  description: "Nico's Music Practice App",
+  title: "Copy Bass",
+  description: "Nico's app for practicing bass guitar",
 };
 
 export default function RootLayout({
