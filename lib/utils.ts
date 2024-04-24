ec7cf52386a5190b79a1ab5dbb791aaac3eebe44
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-24 13:41:45 -0700

    Implement `zipWith` util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+/**
+ * Inspired by `zipWith` from `fp-ts`:
+ * https://gcanti.github.io/fp-ts/modules/Array.ts.html#zipwith
+ *
+ * Applies a function to pairs of elements at the same index in two arrays,
+ * collecting the results in a new array. If one input array is shorter than the
+ * other, excess elements of the longer array will be discarded.
+ */
+export function zipWith<A1, A2, Zipped>(
+  arr1: Array<A1>,
+  arr2: Array<A2>,
+  fn: (a1: A1, a2: A2) => Zipped,
+): Array<Zipped> {
+  const minLength = Math.min(arr1.length, arr2.length);
+  return arr1.slice(0, minLength).map((a1, idx) => fn(a1, arr2[idx]));
+}
