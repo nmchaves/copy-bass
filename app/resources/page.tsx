96bf6e6997e12906a65cfd075ddb72ea3d04b8b9
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-24 18:11:11 -0700

    Add a few links to the Resources page

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -1,5 +1,6 @@
-import { H1 } from "@/components/ui/Heading";
 import { Metadata } from "next";
+import Link from "next/link";
+import { H1 } from "@/components/ui/Heading";
 
 export const metadata: Metadata = {
   title: "Resources",
@@ -9,12 +10,35 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <main className="flex flex-col items-center">
-      <H1 className="mb-4 text-center">Resources</H1>
-      <p>
-        TODO: Add some helpful resources, such as videos/links about how to
-        emulate the tone of various bassists, tutorials on reading sheet music,
-        etc.
-      </p>
+      <H1 className="mb-8 text-center">Resources</H1>
+      <ExternalResourceLink href="https://www.youtube.com/playlist?list=PLHNte-PbVjgXrkBxoMC4xQMqlJ0RNZq5E">
+        Steve Harris Tech 21 Signature Amp Settings
+      </ExternalResourceLink>
+      <ExternalResourceLink href="https://www.musicca.com/circle-of-fifths">
+        Circle of Fifths
+      </ExternalResourceLink>
+      <ExternalResourceLink href="https://www.youtube.com/watch?v=--aVuWXcdTs&list=PLatsInsznGD0dtXAcpvH02QTiFmrF05Tg">
+        Music Theory for Bass Guitar Series (TalkingBass)
+      </ExternalResourceLink>
     </main>
+  );
+}
+
+function ExternalResourceLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Link
+      href={href}
+      rel="noopener noreferrer"
+      target="_blank"
+      className="mb-6 underline underline-offset-4 hover:decoration-2"
+    >
+      {children}
+    </Link>
   );
 }
