fc8d58d4f687c90b0d3e24c5f4943dd7b94b28ee
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-25 18:19:40 -0700

    Add a logo to the header and left-align the nav links

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo_west_ham_maroon.png";
 import { Button } from "@/components/ui/Button";
 
 export const Header = () => {
   return (
-    <header className="flex items-center justify-center py-8">
-      <nav>
+    <header className="pt-8 pb-12">
+      <nav className="flex items-center">
+        <Link href="/">
+          <Image src={logo} height={40} alt="Copy Bass" className="mr-12" />
+        </Link>
         <Link href="/">
           <Button variant="link">Songs</Button>
         </Link>
