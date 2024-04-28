4259237fd36a7bdce342b4ed002ea4ea867230b7
nmchaves <17934735+nmchaves@users.noreply.github.com>
2024-04-27 22:16:00 -0700

    Use the prod domain for `metadataBase` so OG images aren't behind an auth wall

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,26 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Copy Bass",
   description: "Nico's app for practicing bass guitar",
+  // By default, Vercel uses the "deployment URL", i.e. `process.env.VERCEL_URL`
+  // for `metadataBase`.
+  // See https://nextjs.org/docs/app/api-reference/functions/generate-metadata#default-value
+  //
+  // But within the Vercel dashboard, this project's "Deployment Protection"
+  // settings are set to "Standard Protection", which is the default/recommended
+  // setting. As a result, only the up-to-date prod domains (e.g. www.copybass.com)
+  // are publicly accessible. All other URLs (including `VERCEL_URL`) can only
+  // be accessed after authenticating with Vercel. So the OG images would be
+  // behind an auth wall.
+  //
+  // Let's use the prod domain for `metadataBase` so the OG images can be
+  // accessed by other systems (without needing to weaken the project's Deployment
+  // Protection settings).
+  //
+  // For more background, see this related discussion:
+  // https://github.com/vercel/next.js/discussions/50546
+  metadataBase: process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? new URL(`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`)
+    : new URL(`http://localhost:${process.env.PORT || 3000}`),
 };
 
 export default function RootLayout({
